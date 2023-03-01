07675c0b989e58d0ef003540468d8125ebec869e
Mattia <m.bianco@jetop.com>
2023-03-01 16:02:52 +0100

    Init 2 - non funziona tailwind pd

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import Head from "next/head"
+import Image from "next/image"
+import classNames from "classnames"
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-red-300">Prova</div>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "ui-sans-serif"],
+    },
+
+    screens: {
+      phone: "390px",
+      // => @media (min-width: 390px) { ... }
+
+      tablet: "834px",
+      // => @media (min-width: 834px) { ... }
+
+      laptop: "1080px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      colors: { green: "#049F84", red: "#D80D3D", "light-red": "#FD2054" },
+    },
   },
   plugins: [],
 }
