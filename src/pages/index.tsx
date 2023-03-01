3201f1381d44e029fce98cfd4a7ae5c6a197c5ea
Mattia <m.bianco@jetop.com>
2023-03-01 17:35:23 +0100

    structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,25 @@
 import Head from "next/head"
 import Image from "next/image"
 import classNames from "classnames"
+import Clients from "@organisms/Clients"
+import Resources from "@organisms/Resources"
+import Partners from "@organisms/Partners"
+import Activities from "@organisms/Activities"
+import Costs from "@organisms/Costs"
+import Revenue from "@organisms/Revenue"
 
 
 export default function Home() {
   return (
     <>
-      <div className="">Prova</div>
+      <div className="bg-black h-7">
+        <Clients />
+        <Activities />
+        <Partners />
+        {/* <Resources /> Risorse utilizzate di AWS (specifiche) */}
+        <Costs />
+        <Revenue />
+      </div>
     </>
   )
 }
