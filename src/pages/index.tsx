3a1ea90bb0eb6c0cc580bbbf87820146233c2008
Mattia <m.bianco@jetop.com>
2023-03-02 13:06:51 +0100

    Footer and powered by

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head"
 import classNames from "classnames"
 import Hero from "@organisms/Hero"
 import BusinessPlan from "@organisms/Business Plan"
+import Footer from "@organisms/Footer"
 
 const text1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
 const text2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
@@ -19,6 +20,7 @@ export default function Home() {
       <div className="flex flex-col justify-center px-[20px]">
         <Hero text1={text1} text2={text2} text3={text3} />
         <BusinessPlan activitText={activity} clientText={clients} partnersText={partners} CostsRevenueText={costsRevenue} />
+        <Footer />
       </div>
     </>
   )
